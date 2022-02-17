7e39be8dfd96a455c3edf722c8e8189060e2fc02
marc-gomez <gomezme@umich.edu>
2022-02-17 13:55:10 -0500

    JHEMMS-15 Code restructuring

--- server/server.cpp
+++ server/server.cpp
@@ -6,226 +6,130 @@
 #include <fstream>
 #include <httpserver.hpp>
 #include "json.hpp"
+#include "service_resource.hpp"
 
 using namespace std;
-using json = nlohmann::json;
 
 bool verbose = false;
 
-class service_resource: public httpserver::http_resource {
-public:
-	service_resource();
+// Print out server start options
+void usage() {
+	cout << "Usage:" << endl
+		 << "service [-p <port>] [-C <maxConnections>] [-m <memoryLimit>]" << endl
+		 << "[-t <timeoutTime>] [-s [-k <keyFileName>] [-c <certFileName>]] [-v] [-i]" << endl;
+}
 
-	~service_resource();
+struct Settings {
+	uint16_t port;
+	unsigned long maxConns;
+	unsigned long long maxMem;
+	unsigned long timeoutTime;
+	int maxThreads;
+	string key;
+	string cert;
+	string config;
+	bool secure;
+	bool connLimit;
+	bool memLimit;
+	bool useIpv6;
+	bool timeout;
 
-	const std::shared_ptr<httpserver::http_response> render_GET(const httpserver::http_request &req);
-	const std::shared_ptr<httpserver::http_response> render_PUT(const httpserver::http_request &req);
-	const std::shared_ptr<httpserver::http_response> render_POST(const httpserver::http_request &req);
-	const std::shared_ptr<httpserver::http_response> render(const httpserver::http_request &req);
-	const std::shared_ptr<httpserver::http_response> render_HEAD(const httpserver::http_request &req);
-	const std::shared_ptr<httpserver::http_response> render_OPTIONS(const httpserver::http_request &req);
-	const std::shared_ptr<httpserver::http_response> render_CONNECT(const httpserver::http_request &req);
-	const std::shared_ptr<httpserver::http_response> render_DELETE(const httpserver::http_request &req);
+	// Default settings
+	Settings() : port(8080), maxConns(15), maxMem(32768), timeoutTime(180), maxThreads(5),
+				 key("pki/ia.key"), cert("pki/ia.crt"), config("config.json"),
+				 secure(false), connLimit(false), memLimit(false), useIpv6(false),
+				 timeout(false) {}
 };
 
-service_resource::service_resource() { }
+// Get server settings from a json file
+void readFromJSON(int argc, char * const argv[], Settings &s) {
+	nlohmann::json j;
 
-service_resource::~service_resource() { }
+	if (optarg == NULL && optind < argc && argv[optind][0] != '-') {
+		optarg = argv[optind++];
+	}
+	if (optarg != NULL) {
+		s.config = optarg;
+	}
 
-const std::shared_ptr<httpserver::http_response> service_resource::render_GET(const httpserver::http_request &req) {
-	std::cout << "service_resource::render_GET()" << std::endl;
+	ifstream configFile(s.config);
+	if (!configFile.is_open()) {
+		cout << "Failed to open " << s.config << endl;
+		exit(1);
+	}
 
-	if (verbose) std::cout << req;
-	httpserver::string_response* res = new httpserver::string_response("GET response\n", 200);
+	configFile >> j;
 
-	if (verbose) std::cout << *res;
-
-	return std::shared_ptr<httpserver::http_response>(res);
+	if (j.contains("port")) {
+		s.port = j["port"];
+	}
+	if (j.contains("max_threads")) {
+		s.maxThreads = j["max_threads"];
+	}
+	if (j.contains("max_connections")) {
+		s.connLimit = true;
+		s.maxConns = j["max_connections"];
+	}
+	if (j.contains("connection_timeout")) {
+		s.timeout = true;
+		s.timeoutTime = j["connection_timeout"];
+	}
+	if (j.contains("use_ssl")) {
+		s.secure = j["use_ssl"];
+	}
+	if (j.contains("https_mem_key")) {
+		s.key = j["https_mem_key"];
+	}
+	if (j.contains("https_mem_cert")) {
+		s.cert = j["https_mem_cert"];
+	}
+	if (j.contains("memory_limit")) {
+		s.memLimit = true;
+		s.maxMem = j["memory_limit"];
+	}
+	if (j.contains("use_ipv6")) {
+		s.useIpv6 = j["use_ipv6"];
+	}
 }
 
-
-const std::shared_ptr<httpserver::http_response> service_resource::render_PUT(const httpserver::http_request &req) {
-	std::cout << "service_resource::render_PUT()" << std::endl;
-
-	if (verbose) std::cout << req;
-
-	httpserver::string_response* res = new httpserver::string_response("PUT response\n", 200);
-
-	if (verbose) std::cout << *res;
-
-	return std::shared_ptr<httpserver::http_response>(res);
-}
-
-const std::shared_ptr<httpserver::http_response> service_resource::render_POST(const httpserver::http_request &req) {
-	std::cout << "service_resource::render_POST()" << std::endl;
-
-	if (verbose) std::cout << req;
-
-	httpserver::string_response* res = new httpserver::string_response("POST response\n", 200);
-
-	if (verbose) std::cout << *res;
-
-	return std::shared_ptr<httpserver::http_response>(res);
-}
-
-const std::shared_ptr<httpserver::http_response> service_resource::render(const httpserver::http_request &req) {
-	std::cout << "service_resource::render()" << std::endl;
-
-	if (verbose) std::cout << req;
-
-	httpserver::string_response* res = new httpserver::string_response("generic response\n", 200);
-
-	if (verbose) std::cout << *res;
-
-	return std::shared_ptr<httpserver::http_response>(res);
-}
-
-const std::shared_ptr<httpserver::http_response> service_resource::render_HEAD(const httpserver::http_request &req) {
-	std::cout << "service_resource::render_HEAD()" << std::endl;
-
-	if (verbose) std::cout << req;
-
-	httpserver::string_response* res = new httpserver::string_response("HEAD response\n", 200);
-
-	if (verbose) std::cout << *res;
-
-	return std::shared_ptr<httpserver::http_response>(res);
-}
-
-const std::shared_ptr<httpserver::http_response> service_resource::render_OPTIONS(const httpserver::http_request &req) {
-	std::cout << "service_resource::render_OPTIONS()" << std::endl;
-
-	if (verbose) std::cout << req;
-
-	httpserver::string_response* res = new httpserver::string_response("OPTIONS response\n", 200);
-
-	if (verbose) std::cout << *res;
-
-	return std::shared_ptr<httpserver::http_response>(res);
-}
-
-const std::shared_ptr<httpserver::http_response> service_resource::render_CONNECT(const httpserver::http_request &req) {
-	std::cout << "service_resource::render_CONNECT()" << std::endl;
-
-	if (verbose) std::cout << req;
-
-	httpserver::string_response* res = new httpserver::string_response("CONNECT response\n", 200);
-
-	if (verbose) std::cout << *res;
-
-	return std::shared_ptr<httpserver::http_response>(res);
-}
-
-const std::shared_ptr<httpserver::http_response> service_resource::render_DELETE(const httpserver::http_request &req) {
-	std::cout << "service_resource::render_DELETE()" << std::endl;
-
-	if (verbose) std::cout << req;
-
-	httpserver::string_response* res = new httpserver::string_response("DELETE response\n", 200);
-
-	if (verbose) std::cout << *res;
-
-	return std::shared_ptr<httpserver::http_response>(res);
-}
-
-void usage() {
-	std::cout << "Usage:" << std::endl
-			  << "service [-p <port>] [-C <maxConnections>] [-m <memoryLimit>]\n[-t <timeoutTime>] [-s [-k <keyFileName>] [-c <certFileName>]] [-v] [-i]" << std::endl;
-}
-
-int main(int argc, char **argv) {
-	uint16_t port = 8080;
-	unsigned long maxConns = 0;
-	unsigned long long maxMem = 0;
-	unsigned long timeoutTime = 180;
-	int maxThreads = 5;
+// Get server settings
+void getSettings(int argc, char * const argv[], Settings &s) {
 	int c;
-	const char *key = "pki/ia.key";
-	const char *cert = "pki/ia.crt";
-	const char *config = "config.json";
-	bool secure = false;
-	bool connLimit = false;
-	bool memLimit = false;
-	bool useIpv6 = false;
-	bool timeout = false;
-	json j;
-
 	while ((c = getopt(argc, argv, "f::p:C:m:t:k:c:svi?")) != EOF) {
 		switch (c) {
 			case 'f': {
-				if (optarg == NULL && optind < argc && argv[optind][0] != '-') {
-					optarg = argv[optind++];
-				}
-				if (optarg != NULL) {
-					config = optarg;
-				}
-				std::ifstream configFile(config);
-				if (!configFile.is_open()) {
-					std::cout << "Failed to open " << config << std::endl;
-					exit(1);
-				}
-				configFile >> j;
-				if (j.contains("port")) {
-					port = j["port"];
-				}
-				if (j.contains("max_threads")) {
-					maxThreads = j["max_threads"];
-				}
-				if (j.contains("max_connections")) {
-					connLimit = true;
-					maxConns = j["max_connections"];
-				}
-				if (j.contains("connection_timeout")) {
-					timeout = true;
-					timeoutTime = j["connection_timeout"];
-				}
-				if (j.contains("use_ssl")) {
-					secure = true;
-				}
-				if (j.contains("https_mem_key")) {
-					key = string(j["https_mem_key"]).c_str();
-				}
-				if (j.contains("https_mem_cert")) {
-					cert = string(j["https_mem_cert"]).c_str();
-				}
-				if (j.contains("memory_limit")) {
-					memLimit = true;
-					maxMem = j["memory_limit"];
-				}
-				if (j.contains("use_ipv6")) {
-					useIpv6 = true;
-				}
+				readFromJSON(argc, argv, s);
 				break;
 			}
 			case 'p': {
-				port = strtoul(optarg, nullptr, 10);
+				s.port = strtoul(optarg, nullptr, 10);
 				break;
 			}
 			case 'C': {
-				connLimit = true;
-				maxConns = strtoul(optarg, nullptr, 10);
+				s.connLimit = true;
+				s.maxConns = strtoul(optarg, nullptr, 10);
 				break;
 			}
 			case 'm': {
-				memLimit = true;
-				maxMem = strtoull(optarg, nullptr, 10);
+				s.memLimit = true;
+				s.maxMem = strtoull(optarg, nullptr, 10);
 				break;
 			}
 			case 't': {
-				timeout = true;
-				timeoutTime = strtoul(optarg, nullptr, 10);
+				s.timeout = true;
+				s.timeoutTime = strtoul(optarg, nullptr, 10);
 				break;
 			}
 			case 'k': {
-				key = optarg;
+				s.key = optarg;
 				break;
 			}
 			case 'c': {
-				cert = optarg;
+				s.cert = optarg;
 				break;
 			}
 			case 's': {
-				secure = true;
+				s.secure = true;
 				break;
 			}
 			case 'v': {
@@ -233,7 +137,7 @@ int main(int argc, char **argv) {
 				break;
 			}
 			case 'i': {
-				useIpv6 = true;
+				s.useIpv6 = true;
 				break;
 			}
 			default: {
@@ -243,36 +147,38 @@ int main(int argc, char **argv) {
 			}
 		}
 	}
+}
 
+int main(int argc, char **argv) {
+	Settings s;
+	getSettings(argc, argv, s);
 
-
-	std::cout << "Using port " << port << std::endl;
-	if (secure) {
-		std::cout << "Key: " << key << " Certificate: " << cert
-				  << std::endl;
+	cout << "Using port " << s.port << endl;
+	if (s.secure) {
+		cout << "Key: " << s.key << " Certificate: " << s.cert << endl;
 	}
 
 	// Use builder to define webserver configuration options
-	httpserver::create_webserver cw = httpserver::create_webserver(port).max_threads(maxThreads);
+	httpserver::create_webserver cw = httpserver::create_webserver(s.port).max_threads(s.maxThreads);
 
-	if (secure) {
-		cw.use_ssl().https_mem_key(key).https_mem_cert(cert);
+	if (s.secure) {
+		cw.use_ssl().https_mem_key(s.key).https_mem_cert(s.cert);
 	}
 
-	if (connLimit) {
-		cw.max_connections(maxConns);
+	if (s.connLimit) {
+		cw.max_connections(s.maxConns);
 	}
 
-	if (memLimit) {
-		cw.memory_limit(maxMem);
+	if (s.memLimit) {
+		cw.memory_limit(s.maxMem);
 	}
 
-	if (useIpv6) {
+	if (s.useIpv6) {
 		cw.use_ipv6().use_dual_stack();
 	}
 
-	if (timeout) {
-		cw.connection_timeout(timeoutTime);
+	if (s.timeout) {
+		cw.connection_timeout(s.timeoutTime);
 	}
 
 	// Create webserver using the configured options
---
+++ server/service_resource.hpp
@@ -0,0 +1,46 @@
+#ifndef SERVICE_RESOURCE_HPP
+#define SERVICE_RESOURCE_HPP
+
+#include <string>
+#include <unordered_map>
+#include <httpserver.hpp>
+
+#define MY_OPAQUE "11733b200778ce33060f31c9af70a870ba96ddd5"
+
+class service_resource: public httpserver::http_resource {
+private:
+	std::unordered_map<std::string, std::string> users;
+
+public:
+	service_resource() {
+		users["username"] = "password";
+	}
+
+	~service_resource() { }
+
+	const std::shared_ptr<httpserver::http_response> render_GET(const httpserver::http_request &req) {
+		if (req.get_digested_user() == "") {
+			return std::shared_ptr<httpserver::digest_auth_fail_response>
+				(new httpserver::digest_auth_fail_response("FAIL\n",
+					"default realm", MY_OPAQUE, true));
+		}
+		else if (users.find(req.get_digested_user()) == users.end()) {
+			return std::shared_ptr<httpserver::digest_auth_fail_response>
+				(new httpserver::digest_auth_fail_response("Incorrect username or password\n",
+					"default realm", MY_OPAQUE, false));
+		}
+		else {
+			bool reload_nonce = false;
+			if(!req.check_digest_auth("default realm", users[req.get_digested_user()], 300, &reload_nonce)) {
+				return std::shared_ptr<httpserver::digest_auth_fail_response>
+					(new httpserver::digest_auth_fail_response("Incorrect username or password\n",
+						"default realm", MY_OPAQUE, reload_nonce));
+			}
+		}
+		std::cout << "GET success, user: " << req.get_digested_user() << std::endl;
+		return std::shared_ptr<httpserver::string_response>
+			(new httpserver::string_response("SUCCESS\n", 200, "text/plain"));
+	}
+};
+
+#endif
